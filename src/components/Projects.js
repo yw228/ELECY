b1fc9ca44799b0c2ae3baa03ec59bf4148008dfa
Yiqi Wang <yw228@byu.edu>
2024-07-11 14:57:58 -0700

    run

---
+++ src/components/Projects.js
@@ -0,0 +1,82 @@
+import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
+import { ProjectCard } from "./ProjectCard";
+import projImg1 from "../assets/img/github-mark-white.svg";
+import projImg2 from "../assets/img/github-mark-white.svg";
+import projImg3 from "../assets/img/project-img3.png";
+import colorSharp2 from "../assets/img/color-sharp2.png";
+import 'animate.css';
+import TrackVisibility from 'react-on-screen';
+
+export const Projects = () => {
+
+  const projects = [
+    {
+      title: "Chess Music",
+      description: "Application in Flutter",
+      imgUrl: projImg1,
+      url: "https://yw228.github.io/assets/videos/ChessMusic.mp4"
+    },
+    {
+      title: "2 Player Game",
+      description: "Design & Development",
+      imgUrl: projImg2,
+      url: "https://game.blackman.zip/",
+    },
+  ];
+
+  return (
+    <section className="project" id="projects">
+      <Container>
+        <Row>
+          <Col size={12}>
+            <TrackVisibility>
+              {({ isVisible }) =>
+              <div className={isVisible ? "animate__animated animate__fadeIn": ""}>
+                <h2>Projects</h2>
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                <Tab.Container id="projects-tabs" defaultActiveKey="first">
+                  <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
+                  </Nav>
+                  {/*
+                     <Nav.Item>
+                     <Nav.Link eventKey="first">Tab 1</Nav.Link>
+                     </Nav.Item>
+                     <Nav.Item>
+                     <Nav.Link eventKey="second">Tab 2</Nav.Link>
+                     </Nav.Item>
+                     <Nav.Item>
+                     <Nav.Link eventKey="third">Tab 3</Nav.Link>
+                     </Nav.Item >
+                  */}
+                  <Tab.Content id="slideInUp" className={isVisible ? "animate__animated animate__slideInUp" : ""}>
+                    <Tab.Pane eventKey="first">
+                      <Row>
+                        {
+                          projects.map((project, index) => {
+                            return (
+                              <ProjectCard
+                                key={index}
+                                {...project}
+                                />
+                            )
+                          })
+                        }
+                      </Row>
+                    </Tab.Pane>
+                    <Tab.Pane eventKey="section">
+                      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.</p>
+                    </Tab.Pane>
+                    <Tab.Pane eventKey="third">
+                      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.</p>
+                    </Tab.Pane>
+                  </Tab.Content>
+                </Tab.Container>
+              </div>}
+            </TrackVisibility>
+          </Col>
+        </Row>
+      </Container>
+      <img className="background-image-right" src={colorSharp2}></img>
+    </section>
+  )
+}
